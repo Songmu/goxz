28ea99d9d51c7ef83be5f6b0c7da3c8a90263c15
Songmu <y.songmu@gmail.com>
2017-12-26 01:24:30 +0900

    enhance help output

--- cli.go
+++ cli.go
@@ -2,8 +2,10 @@ package goxz
 
 import (
 	"flag"
+	"fmt"
 	"io"
 	"log"
+	"runtime"
 )
 
 type cli struct {
@@ -26,18 +28,30 @@ func (cl *cli) parseArgs(args []string) (*goxz, error) {
 	gx := &goxz{}
 	fs := flag.NewFlagSet("goxz", flag.ContinueOnError)
 	fs.SetOutput(cl.errStream)
+	fs.Usage = func() {
+		fmt.Fprintf(cl.errStream, `goxz - Just do cross building and archiving go tools conventionally
+
+Version: %s (rev: %s/%s)
+
+Synopsis:
+    %% gozx -v 0.0.1 -os=linux,darwin -arch=amd64 ./cmd/mytool [...]
+
+Options:
+`, version, revision, runtime.Version())
+		fs.PrintDefaults()
+	}
 
 	fs.StringVar(&gx.name, "n", "", "Application name. By default this is the directory name.")
 	fs.StringVar(&gx.dest, "d", "goxz", "Destination directory")
-	fs.StringVar(&gx.version, "pv", "", "Package version")
-	fs.StringVar(&gx.output, "o", "", "output")
+	fs.StringVar(&gx.version, "pv", "", "Package version (optional)")
+	fs.StringVar(&gx.output, "o", "", "output (optional)")
 	fs.StringVar(&gx.os, "os", "", "Specify OS (default is 'linux darwin windows')")
 	fs.StringVar(&gx.arch, "arch", "", "Specify Arch (default is 'amd64')")
 	// TODO: fs.StringVar(&gx.buildConstraints, "build", "", "Specify build constraints")
 	fs.StringVar(&gx.buildLdFlags, "build-ldflags", "", "arguments to pass on each go tool link invocation")
 	fs.StringVar(&gx.buildTags, "build-tags", "", "a space-separated list of build `tags`")
 	fs.BoolVar(&gx.zipAlways, "z", false, "zip always")
-	fs.StringVar(&gx.projDir, "C", "", "specify the project directory")
+	fs.StringVar(&gx.projDir, "C", "", "specify the project directory. cwd by default")
 
 	fs.BoolVar(&gx.work, "work", false, "[for debug] print the name of the temporary work directory and do not delete it when exiting.")
 
