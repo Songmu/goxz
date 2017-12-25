e451ace7e689d132ebc3153eeb11a98b468cd597
Songmu <y.songmu@gmail.com>
2017-12-25 17:17:23 +0900

    separate cli.go

--- goxz.go
+++ goxz.go
@@ -3,7 +3,6 @@ package goxz
 import (
 	"flag"
 	"go/build"
-	"io"
 	"io/ioutil"
 	"log"
 	"os"
@@ -11,10 +10,6 @@ import (
 	"regexp"
 	"strings"
 )
-
-type cli struct {
-	outStream, errStream io.Writer
-}
 
 const (
 	exitCodeOK = iota
@@ -47,80 +42,6 @@ type goxz struct {
 	projDir   string
 	workDir   string
 	resources []string
-}
-
-func (cl *cli) run(args []string) error {
-	log.SetOutput(cl.errStream)
-	log.SetPrefix("[goxz] ")
-	log.SetFlags(0)
-
-	gx, err := cl.parseArgs(args)
-	if err != nil {
-		return err
-	}
-	if gx.projDir != "" {
-		prev, err := filepath.Abs(".")
-		if err != nil {
-			return err
-		}
-		err = os.Chdir(gx.projDir)
-		if err != nil {
-			return err
-		}
-		defer os.Chdir(prev)
-	}
-	err = gx.init()
-	if err != nil {
-		return err
-	}
-	err = gx.prepareWorkdir()
-	if err != nil {
-		return err
-	}
-	defer func() {
-		if !gx.work {
-			os.RemoveAll(gx.workDir)
-		}
-	}()
-	if gx.work {
-		log.Printf("working dir: %s\n", gx.workDir)
-	}
-
-	for _, bdr := range gx.builders() {
-		// XXX use goroutine and sync.ErrorGroup
-		_, _ = bdr.build()
-	}
-
-	return nil
-}
-
-func (cl *cli) parseArgs(args []string) (*goxz, error) {
-	gx := &goxz{}
-	fs := flag.NewFlagSet("goxz", flag.ContinueOnError)
-	fs.SetOutput(cl.errStream)
-
-	fs.StringVar(&gx.name, "n", "", "Application name. By default this is the directory name.")
-	fs.StringVar(&gx.dest, "d", "goxz", "Destination directory")
-	fs.StringVar(&gx.version, "pv", "", "Package version")
-	fs.StringVar(&gx.output, "o", "", "output")
-	fs.StringVar(&gx.os, "os", "", "Specify OS (default is 'linux darwin windows')")
-	fs.StringVar(&gx.arch, "arch", "", "Specify Arch (default is 'amd64')")
-	fs.StringVar(&gx.buildLdFlags, "build-ldflags", "", "arguments to pass on each go tool link invocation")
-	fs.StringVar(&gx.buildTags, "build-tags", "", "a space-separated list of build `tags`")
-	fs.BoolVar(&gx.zipAlways, "zip", false, "zip always")
-
-	fs.StringVar(&gx.projDir, "C", "", "[for debug] change directory")
-	fs.BoolVar(&gx.work, "work", false, "[for debug] print the name of the temporary work directory and do not delete it when exiting.")
-
-	err := fs.Parse(args)
-	if err != nil {
-		return nil, err
-	}
-	gx.pkgs = fs.Args()
-	if len(gx.pkgs) == 0 {
-		gx.pkgs = append(gx.pkgs, ".")
-	}
-	return gx, nil
 }
 
 func (gx *goxz) init() error {
